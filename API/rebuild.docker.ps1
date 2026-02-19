0859f67258b7c7c1d4f6d4441ebade3ae4559d19
ivarv <ivarvisser@hotmail.com>
2026-02-19 15:00:31 +0100

    Copied from old branch; Swagger autolaunch docker script

--- API/rebuild.docker.ps1
+++ API/rebuild.docker.ps1
@@ -1,22 +1,27 @@
-﻿# powershell
+﻿﻿# powershell
 param(
-    [switch]$SkipTests,
-    [switch]$skiptest,
-    [switch]$nonDestructive
+[switch]$SkipTests,
+[switch]$skiptest,
+[switch]$nonDestructive
 )
 $ErrorActionPreference = "Stop"
 
 dotnet.exe build
-if ($LASTEXITCODE -ne 0) {
-Write-Host "==> Build failed."
-exit $LASTEXITCODE
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Host "==> Build failed."
+    exit $LASTEXITCODE
 }
-Write-Host "==> Bringing down compose stack (remove volumes)..."
-if ($nonDestructive) {
-Write-Host "==> Skipping volume removal due to non-destructive flag."
-docker compose -f .\docker-compose.yml down
-} else {
-docker compose -f .\docker-compose.yml down -v
+if ($nonDestructive)
+{
+    Write-Host "==> Skipping volume removal due to non-destructive flag."
+    docker compose -f .\docker-compose.yml down
+}
+else
+{
+    Write-Host "==> Bringing down compose stack (remove volumes)..."
+
+    docker compose -f .\docker-compose.yml down -v
 }
 Write-Host "==> Bringing up compose stack (rebuild images)..."
 docker compose -f docker-compose.yml up --build
