43d53015095c3f0ddb0a82693f1eeda318a04025
Giel van Gaal <giel@lenovo-giel.home>
2026-02-28 12:23:44 +0100

    Added styleguide and stuff

--- WA/Program.cs
+++ WA/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
 using MudBlazor.Services;
 using WA;
 using WA.ApiClients;
@@ -12,13 +13,25 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
+// MUDBLAZOR
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+    config.SnackbarConfiguration.MaxDisplayedSnackbars = 3;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+});
+
+// AUTH
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
+// HTTP
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri("https://p3api-prod.gielvangaal.dev/") });
     // new HttpClient { BaseAddress = new Uri("https://p3api-acc.gielvangaal.dev/") });
     // new HttpClient { BaseAddress = new Uri("http://localhost:8080/") });
+
+// WA API/SERVICES
 builder.Services.AddScoped<IUserApi, UserApi>();
 builder.Services.AddScoped<LayoutStateService>();
 
