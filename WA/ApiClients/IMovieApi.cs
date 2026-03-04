92799240a48bc8827802cf8fbfb072501f75e160
Ruben Leenknegt <r.leenknegt@pm.me>
2026-03-04 16:14:44 +0100

    CN-60: Setup frontend for movie list and edit page

---
+++ WA/ApiClients/IMovieApi.cs
@@ -0,0 +1,10 @@
+﻿// using SharedLibrary.DTOs.Responses;
+//
+// namespace WA.ApiClients;
+//
+// public interface IMovieApiClient
+// {
+//     Task<List<MovieDto>?> GetAllMoviesAsync();
+//     Task<MovieDto?> GetMovieByIdAsync(int id);
+//     Task<bool> DeleteMovieAsync(int id);
+// }
