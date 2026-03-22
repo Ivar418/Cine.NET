9694154bd3148e7337afd93f76d6d35becde394d
BartKuijs <bartkuijs@live.nl>
2026-03-22 17:19:05 +0100

    CN-61: Fix ShowingMapper instead of ShowingDto

--- SharedLibrary/DTOs/Models/Showing.cs
+++ SharedLibrary/DTOs/Models/Showing.cs
@@ -6,16 +6,9 @@ using System.Text;
 namespace SharedLibrary.DTOs.Models
 {
     public record ShowingStateDto(
-    ShowingDto Showing,
+    Showing Showing,
     List<SeatInfo> AllSeats,       // derived from the stored Auditorium-layout snapshot
     HashSet<string> OccupiedKeys    // "row-col" of confirmed seats
-    );
-
-    public record ShowingDto(
-    int Id,
-    Movie Movie,
-    AuditoriumDto Auditorium,
-    DateTimeOffset StartsAt
     );
 
     public record CreateShowingRequest(int MovieId, int AuditoriumId, DateTimeOffset StartsAt, bool Is3D = false);
