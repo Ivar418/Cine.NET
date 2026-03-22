daa9bab7b8753157686d4af6555672e741049e5c
ivarv <ivarvisser@hotmail.com>
2026-03-22 16:52:26 +0100

    CN-120 written a converter for the showing startsAt property to string and a parser for getting it. This makes it possible to use DATETIMEOFFSET in the code and store it as a string in the DB. Mysql does not support datetimeoffset

--- API/src/Infrastructure/Database/ApiDbContext.cs
+++ API/src/Infrastructure/Database/ApiDbContext.cs
@@ -6,86 +6,96 @@ using System.Collections.Generic;
 using SharedLibrary.Domain.Entities;
 
 public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options)
+        : base(options)
     {
-
-        public ApiDbContext(DbContextOptions<ApiDbContext> options)
-            : base(options) { }
-
-
-        public DbSet<User> Users => Set<User>();
-        public DbSet<Movie> Movies => Set<Movie>();
-        public DbSet<Photo> Photos => Set<Photo>();
-        public DbSet<Genre> Genres => Set<Genre>();
-        public DbSet<Ticket> Tickets => Set<Ticket>();
-        public DbSet<Auditorium> Auditoriums => Set<Auditorium>();
-        public DbSet<Showing> Showings => Set<Showing>();
-        public DbSet<Reservation> Reservations => Set<Reservation>();
-        public DbSet<Order> Orders => Set<Order>();
-        public DbSet<OrderTicket> OrderTickets => Set<OrderTicket>();
-        public DbSet<PaymentMethod> PaymentMethods => Set<PaymentMethod>();
-        
-        // Pricing related entities
-        public DbSet<TicketType> TicketTypes => Set<TicketType>();
-        public DbSet<PricingConfig> PricingConfigs => Set<PricingConfig>();
-        public DbSet<PricingOption> PricingOptions => Set<PricingOption>();
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<User>().ToTable("users");
-            modelBuilder.Entity<Movie>().ToTable("movies");
-            modelBuilder.Entity<Photo>().ToTable("photos");
-            modelBuilder.Entity<Ticket>().ToTable("tickets");
-            modelBuilder.Entity<Auditorium>().ToTable("auditoriums");
-            modelBuilder.Entity<Showing>().ToTable("showings");
-            modelBuilder.Entity<Reservation>().ToTable("reservations");
-            modelBuilder.Entity<Order>().ToTable("orders");
-            modelBuilder.Entity<Order>()
-                .Property(o => o.TotalAmount)
-                .HasPrecision(10, 2);
-            modelBuilder.Entity<Order>()
-                .HasOne(o => o.PaymentMethodNavigation)
-                .WithMany(pm => pm.Orders)
-                .HasForeignKey(o => o.PaymentMethod)
-                .HasPrincipalKey(pm => pm.Code)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<OrderTicket>().ToTable("order_tickets");
-            modelBuilder.Entity<OrderTicket>()
-                .HasKey(ot => new { ot.OrderId, ot.TicketId });
-            modelBuilder.Entity<OrderTicket>()
-                .HasOne(ot => ot.Order)
-                .WithMany(o => o.OrderTickets)
-                .HasForeignKey(ot => ot.OrderId);
-            modelBuilder.Entity<OrderTicket>()
-                .HasOne(ot => ot.Ticket)
-                .WithMany()
-                .HasForeignKey(ot => ot.TicketId);
-
-                modelBuilder.Entity<PaymentMethod>().ToTable("payment_methods");
-                modelBuilder.Entity<PaymentMethod>()
-                    .HasKey(pm => pm.Code);
-                modelBuilder.Entity<PaymentMethod>()
-                    .Property(pm => pm.Code)
-                    .HasMaxLength(20);
-                modelBuilder.Entity<PaymentMethod>()
-                    .Property(pm => pm.DisplayName)
-                    .HasMaxLength(50);
-
-                // Pricing related entities
-                modelBuilder.Entity<TicketType>().ToTable("ticket_types");
-                modelBuilder.Entity<TicketType>()
-                    .Property(p => p.Discount)
-                    .HasPrecision(10, 2);
-                modelBuilder.Entity<PricingConfig>().ToTable("pricing_configs");
-                modelBuilder.Entity<PricingConfig>()
-                    .Property(p => p.Value)
-                    .HasPrecision(10, 2);
-                modelBuilder.Entity<PricingOption>().ToTable("pricing_options");
-                modelBuilder.Entity<PricingOption>()
-                    .Property(p => p.PriceModifier)
-                    .HasPrecision(10, 2);
-                modelBuilder.Entity<Genre>().ToTable("genres");
-
-        }
     }
+
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<Photo> Photos => Set<Photo>();
+    public DbSet<Genre> Genres => Set<Genre>();
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<Auditorium> Auditoriums => Set<Auditorium>();
+    public DbSet<Showing> Showings => Set<Showing>();
+    public DbSet<Reservation> Reservations => Set<Reservation>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderTicket> OrderTickets => Set<OrderTicket>();
+    public DbSet<PaymentMethod> PaymentMethods => Set<PaymentMethod>();
+
+    // Pricing related entities
+    public DbSet<TicketType> TicketTypes => Set<TicketType>();
+    public DbSet<PricingConfig> PricingConfigs => Set<PricingConfig>();
+    public DbSet<PricingOption> PricingOptions => Set<PricingOption>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>().ToTable("users");
+        modelBuilder.Entity<Movie>().ToTable("movies");
+        modelBuilder.Entity<Photo>().ToTable("photos");
+        modelBuilder.Entity<Ticket>().ToTable("tickets");
+        modelBuilder.Entity<Auditorium>().ToTable("auditoriums");
+        modelBuilder.Entity<Showing>().ToTable("showings");
+        modelBuilder.Entity<Reservation>().ToTable("reservations");
+        modelBuilder.Entity<Order>().ToTable("orders");
+        modelBuilder.Entity<Order>()
+            .Property(o => o.TotalAmount)
+            .HasPrecision(10, 2);
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.PaymentMethodNavigation)
+            .WithMany(pm => pm.Orders)
+            .HasForeignKey(o => o.PaymentMethod)
+            .HasPrincipalKey(pm => pm.Code)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<OrderTicket>().ToTable("order_tickets");
+        modelBuilder.Entity<OrderTicket>()
+            .HasKey(ot => new { ot.OrderId, ot.TicketId });
+        modelBuilder.Entity<OrderTicket>()
+            .HasOne(ot => ot.Order)
+            .WithMany(o => o.OrderTickets)
+            .HasForeignKey(ot => ot.OrderId);
+        modelBuilder.Entity<OrderTicket>()
+            .HasOne(ot => ot.Ticket)
+            .WithMany()
+            .HasForeignKey(ot => ot.TicketId);
+
+        modelBuilder.Entity<PaymentMethod>().ToTable("payment_methods");
+        modelBuilder.Entity<PaymentMethod>()
+            .HasKey(pm => pm.Code);
+        modelBuilder.Entity<PaymentMethod>()
+            .Property(pm => pm.Code)
+            .HasMaxLength(20);
+        modelBuilder.Entity<PaymentMethod>()
+            .Property(pm => pm.DisplayName)
+            .HasMaxLength(50);
+
+        // Pricing related entities
+        modelBuilder.Entity<TicketType>().ToTable("ticket_types");
+        modelBuilder.Entity<TicketType>()
+            .Property(p => p.Discount)
+            .HasPrecision(10, 2);
+        modelBuilder.Entity<PricingConfig>().ToTable("pricing_configs");
+        modelBuilder.Entity<PricingConfig>()
+            .Property(p => p.Value)
+            .HasPrecision(10, 2);
+        modelBuilder.Entity<PricingOption>().ToTable("pricing_options");
+        modelBuilder.Entity<PricingOption>()
+            .Property(p => p.PriceModifier)
+            .HasPrecision(10, 2);
+        modelBuilder.Entity<Genre>().ToTable("genres");
+
+        modelBuilder.Entity<Showing>().Property(e => e.StartsAt).HasConversion(v => v.ToString(), v =>
+            (DateTimeOffset)System.DateTimeOffset.ParseExact(
+                v,
+                "O", // Exact ISO 8601 round-trip format
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None
+            )
+        );
+    }
+}
