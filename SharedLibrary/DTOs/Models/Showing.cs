11a6e6c06a4a69c29b23685a2f764f566a8668b6
BartKuijs <bartkuijs@live.nl>
2026-03-22 16:48:27 +0100

    CN-61: ShowingDto fix

--- SharedLibrary/DTOs/Models/Showing.cs
+++ SharedLibrary/DTOs/Models/Showing.cs
@@ -6,9 +6,16 @@ using System.Text;
 namespace SharedLibrary.DTOs.Models
 {
     public record ShowingStateDto(
-    Showing Showing,
+    ShowingDto Showing,
     List<SeatInfo> AllSeats,       // derived from the stored Auditorium-layout snapshot
     HashSet<string> OccupiedKeys    // "row-col" of confirmed seats
+    );
+
+    public record ShowingDto(
+    int Id,
+    Movie Movie,
+    AuditoriumDto Auditorium,
+    DateTimeOffset StartsAt
     );
 
     public record CreateShowingRequest(int MovieId, int AuditoriumId, DateTimeOffset StartsAt, bool Is3D = false);
