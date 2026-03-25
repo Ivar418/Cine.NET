fcdab9d1c889398ece8774d08e84442c0687fd7d
Ivar418 <ivarvisser@hotmail.com>
2026-03-25 21:18:29 +0100

    CN-34 add mail subscription repo

---
+++ API/src/Repositories/Implementations/MailSubscriptionRepository.cs
@@ -0,0 +1,44 @@
+﻿using API.Domain.Model;
+using API.Infrastructure.Database;
+using API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories.Implementations;
+
+public class MailSubscriptionRepository : IMailSubscriptionRepository
+{
+    private readonly ApiDbContext _db;
+
+    public MailSubscriptionRepository(ApiDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<bool> AddAsync(string email)
+    {
+        _db.EmailSubscriptions.Add(new EmailSubscription { Email = email });
+        await _db.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> RemoveAsync(string email)
+    {
+        _db.EmailSubscriptions.Remove(new EmailSubscription { Email = email });
+        await _db.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> ExistsAsync(string email)
+    {
+        if (await _db.EmailSubscriptions.AnyAsync(e => e.Email == email))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<IEnumerable<EmailSubscription>> GetAllAsync()
+    {
+        return await _db.EmailSubscriptions.ToListAsync();
+    }
+}
