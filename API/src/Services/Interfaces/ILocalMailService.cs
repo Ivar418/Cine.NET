50f6378e5b5ce6728570e8291e7ec004cd4e4f05
Ivar418 <ivarvisser@hotmail.com>
2026-03-25 21:18:54 +0100

    CN-34 add mail service

---
+++ API/src/Services/Interfaces/ILocalMailService.cs
@@ -0,0 +1,13 @@
+﻿using API.Domain.Model;
+using MimeKit;
+
+namespace API.Services.Interfaces;
+
+public interface ILocalMailService
+{
+    public Task<bool> SendEmailToSubscribersAsync(TextPart emailContent, string fromName, string subject);
+    public Task<bool> AddAsync(string email);
+    public Task<bool> RemoveAsync(string email);
+    public Task<bool> ExistsAsync(string email);
+    public Task<IEnumerable<EmailSubscription>> GetAllAsync();
+}
