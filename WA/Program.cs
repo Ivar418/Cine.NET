c6a8b896d9aa430f21750d5c49f3dbf3d8c3a9a8
BartKuijs <bartkuijs@live.nl>
2026-03-28 22:33:38 +0100

    CN-151: Setup Localization.
    Localization can be implemented everywhere but there need to be a fix for changing the language. This only works in the header.

--- WA/Program.cs
+++ WA/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
 
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
@@ -13,6 +15,9 @@ using WA.Services.Http.Interfaces;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//LOCALIZATION
+builder.Services.AddLocalization();
 
 // MUDBLAZOR
 builder.Services.AddMudServices(config =>
@@ -40,4 +45,14 @@ builder.Services.AddScoped<IShowingApi, ShowingApi>();
 builder.Services.AddScoped<IAuditoriumApi, AuditoriumApi>();
 builder.Services.AddScoped<ISeatFinderApiClient, SeatFinderApiService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+// Standaard cultuur instellen
+var culture = new CultureInfo("nl");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
+
+
+//await builder.Build().RunAsync();
