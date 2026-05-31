f71ea2f0c1618bd87fa6fa20244df7b292a5697d
ivarv <ivarvisser@hotmail.com>
2026-05-31 17:15:20 +0200

    Remove unused `GetAll` and `GetById` user endpoints from `UserController`.

--- API/src/Controllers/UserController.cs
+++ API/src/Controllers/UserController.cs
@@ -17,32 +17,6 @@ public class UserController : ControllerBase {
         _logger = logger;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAll() {
-        try {
-            var users = await _userService.GetAllUsersAsync();
-
-            return users switch {
-                { IsFailure: true } => StatusCode(500, new { error = "An error occurred" }),
-                { IsSuccess: true } => Ok(UserMapper.ToResponses(users.Value!)),
-                _ => StatusCode(500, new { error = "Unexpected result" })
-            };
-        }
-        catch (Exception) {
-            return StatusCode(500, new { error = "An error occurred" });
-        }
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(int id) {
-        var user = await _userService.GetUserByIdAsync(id);
-
-        if (user == null)
-            return NotFound();
-
-        var response = UserMapper.ToResponse(user.Value!);
-        return Ok(response);
-    }
 
     [Route("register")]
     [HttpPost]
