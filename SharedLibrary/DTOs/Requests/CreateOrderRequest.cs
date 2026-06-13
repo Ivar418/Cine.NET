e51d04391753861f6e503a1645d4e1a1e3783f80
ivarv <ivarvisser@hotmail.com>
2026-06-13 21:11:53 +0200

    CAP-54 Add UserId field to orders and requests to support user association, preserving guest orders compatibility.

--- SharedLibrary/DTOs/Requests/CreateOrderRequest.cs
+++ SharedLibrary/DTOs/Requests/CreateOrderRequest.cs
@@ -1,8 +1,10 @@
+using SharedLibrary.Domain.Entities;
+
 namespace SharedLibrary.DTOs.Requests;
 
-public class CreateOrderRequest
-{
+public class CreateOrderRequest {
     public required string OrderType { get; set; }
     public required string PaymentMethod { get; set; }
     public required List<TicketRequest> Tickets { get; set; }
+    public int? UserId { get; set; }
 }
